e182df0f3e9171e6d4fd13a1cedada6e54d89252
Miguel Ruiz Manzano <mruiz@linux.com>
2024-05-06 10:07:52 -0400

    Revert "Variables refactoring"
    This reverts commit daf60db6f68603a8cc51d68a1235f354d1b69659.

--- modules/blog/variables.tf
+++ modules/blog/variables.tf
@@ -1,3 +1,18 @@
+variable "project_id" {
+  type = string
+  default = "terraform-422214"
+}
+
+variable "region" {
+  type = string
+  default = "southamerica-west1"
+}
+
+variable "network_name" {
+  type = string
+  default = "app"
+}
+
 variable "network_ip_range" {
   type = string
   default = "10.2.0.0/16"
@@ -11,6 +26,11 @@ variable "image_project" {
 variable "image_family" {
   type = string
   default = "ubuntu-2204-lts"
+}
+
+variable "app_name" {
+  type = string
+  default = "blog"
 }
 
 variable "machine_type" {
